8e7929659948f3cf0f7e60dfeef4e1fac818213d
kelzbiggie01 <kelzbiggie@gmail.com>
2022-05-10 19:46:01 +0300

    first commit

---
+++ app/src/main/java/com/example/jujufood/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.jujufood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final long SPLASH_TIMER = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //set up notitle
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //set up full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+    }
+}
