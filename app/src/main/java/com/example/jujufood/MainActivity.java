3ca502f2b47c66e2a3effc5b180debd93f143b8a
kelzbiggie01 <kelzbiggie@gmail.com>
2022-05-10 20:11:31 +0300

    added navigation activity

--- app/src/main/java/com/example/jujufood/MainActivity.java
+++ app/src/main/java/com/example/jujufood/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.jujufood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -20,5 +22,15 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMER);
     }
 }
